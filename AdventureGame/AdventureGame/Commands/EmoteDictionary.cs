1bde40fd1bc8be54dbc5d423236d16c53c2dd9e6
Carder Williams <sprtsfreek989@cox.net>
2012-05-22 11:03:52 -0500

    renames for easier readability

---
+++ AdventureGame/AdventureGame/Commands/EmoteDictionary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventureGame.Commands
+{
+    class EmoteCommands : ICommand
+    {
+        private Dictionary<string, string> EmoteDictionary = new Dictionary<string, string>();
+
+        public EmoteCommands()
+        {
+            EmoteDictionary.Add("dance", "You do a jig");
+            EmoteDictionary.Add("jump", "You jump and land on a broken board. You fall 50 feet through the floor. You loose.");
+            EmoteDictionary.Add("laugh", "What is so funny?");
+            EmoteDictionary.Add("wave", "Wave at who?");
+        }
+
+        public void Execute(string commandPass)
+        {
+            string eDictionaryResponse = EmoteDictionary[commandPass];
+            Console.WriteLine(eDictionaryResponse);
+        }
+    }
+}
